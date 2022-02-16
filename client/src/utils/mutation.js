da0905a5e5dfd5212d67c8316b22109682c4c87e
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2022-02-16 02:49:48 -0500

    refactoring the app

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -3,6 +3,7 @@
  export const LOGIN_USER = gql`
     mutation login($username: String!, $password: String!) {
         login(username :$username, password: $password) {
+            token
             user {
                 _id
                 username
@@ -14,6 +15,7 @@
  export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
+      token
       user {
         _id
         username
